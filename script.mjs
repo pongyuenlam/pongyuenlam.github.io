039ec9f0a48952be91ce86f545151577f153c6f2
pongyuenlam <121112796+pongyuenlam@users.noreply.github.com>
2024-08-03 20:03:35 +0800

    Add files via upload

---
+++ script.mjs
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    console.log('request was mde: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var myObj = {
+        name: 'Howard',
+        gender: 'Male',
+        city: 'Hong Kong'
+    };
+    res.end(JSON.stringify(myObj));
+});
+
+let d = new Date();
+document.body.innerHTML = "<h1>hello " + d + "</h1>"
+
+server.listen(3000, '127.0.0.1');
+console.log('listen to port 3000');
