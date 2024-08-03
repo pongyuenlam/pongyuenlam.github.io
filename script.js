4c71d80f197097ff403b8776a83917c916f5e823
pongyuenlam <121112796+pongyuenlam@users.noreply.github.com>
2024-08-03 20:00:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-import http from 'http';
-import fs from 'fs';
+import { createServer } from 'node:http';
 
-var server = http.createServer(function(req, res){
-    console.log('request was mde: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var myObj = {
-        name: 'Howard',
-        gender: 'Male',
-        city: 'Hong Kong'
-    };
-    res.end(JSON.stringify(myObj));
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World2');
 });
 
-let d = new Date();
-document.body.innerHTML = "<h1>hello " + d + "</h1>"
-
-server.listen(3000, '127.0.0.1');
-console.log('listen to port 3000');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
