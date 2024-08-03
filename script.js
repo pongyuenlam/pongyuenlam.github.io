0aaa24e3f9ba60134f5c7bebda9d89e709871f88
pongyuenlam <121112796+pongyuenlam@users.noreply.github.com>
2024-08-03 17:02:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var fs = require('fs');
+import http from 'http';
+import fs from 'fs';
 
 var server = http.createServer(function(req, res){
     console.log('request was mde: ' + req.url);
