13a21a061f53a5ec23458c9eb004822741d8673a
pongyuenlam <121112796+pongyuenlam@users.noreply.github.com>
2024-08-03 16:24:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    console.log('request was mde: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var myObj = {
+        name: 'Howard',
+        gender: 'Male',
+        city: 'Hong Kong'
+    };
+    res.end(JSON.stringify(myObj));
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('listen to port 3000');
